d7a393163a92ee989b6b4959c5573d8a0974a3b1
terra456 <terra.456@yandex.ru>
2023-02-09 22:50:13 +0500

    feat: install nodemon and config it

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,5 +4,6 @@ const app = express();
 const port = 5000;
 app.get('/', (request, response) => {
   response.send('Hello world!');
+  
 });
 app.listen(port, () => console.log(`Running on port ${port}`));
