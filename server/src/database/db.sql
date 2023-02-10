e69e608a9ed909e64ae473b7eeb00ace65fd5a23
terra456 <terra.456@yandex.ru>
2023-02-10 14:57:07 +0500

    feat: create file to create DB in command line

---
+++ server/src/database/db.sql
@@ -0,0 +1,32 @@
+create TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (50) NOT NULL,
+  password VARCHAR (50),
+  avatar VARCHAR (255),
+  settings VARCHAR,
+  level smallint,
+  score int,
+  date timestamp
+);
+
+create TABLE wins (
+  id SERIAL PRIMARY KEY,
+  user_id int,
+  date timestamp NOT NULL,
+  level smallint NOT NULL,
+  dificulty VARCHAR (50) NOT NULL,
+  score int NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+create TABLE saved (
+  id SERIAL PRIMARY KEY,
+  user_id int,
+  name  VARCHAR (50) NOT NULL,
+  date timestamp NOT NULL,
+  level smallint NOT NULL,
+  dificulty VARCHAR (50) NOT NULL,
+  score int NOT NULL,
+  state VARCHAR NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
