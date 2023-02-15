fa31a423bf9987bd8e4b903d9ddc6ff5efa983f7
terra456 <terra.456@yandex.ru>
2023-02-15 11:42:43 +0500

    feat: implement collusion detection

--- client/src/game/GameCanvas.ts
+++ client/src/game/GameCanvas.ts
@@ -8,14 +8,16 @@ class GameCanvas {
   scaledCanvas: { width: number, height: number };
   canvas: HTMLCanvasElement;
   context: CanvasRenderingContext2D;
+  scale: number;
 
   constructor (parentNode: HTMLElement, width = 1024, height = 576) {
     this.canvas = document.createElement('canvas');
     this.canvas.width = width;
     this.canvas.height = height;
+    this.scale = 4;
     this.scaledCanvas = {
-      width: width / 4,
-      height: height / 4
+      width: width / this.scale,
+      height: height / this.scale
     };
     parentNode.appendChild(this.canvas);
     const context = this.canvas.getContext('2d');
@@ -29,12 +31,12 @@ class GameCanvas {
       left: false,
       right: false
     };
-    const player = new Player(context, { x: 10, y: 10 }, { width: this.canvas.width, height: this.canvas.height });
-    const player2 = new Player(context, { x: 50, y: 50 }, { width: this.canvas.width, height: this.canvas.height });
-    const background = new Sprite(context, { x: 0, y: 0 }, '../assets/background.png');
     const collusionField = new CollusionField(context, 16);
     const floorCollusions = collusionField.generateCollusionBlocks(floorCollisions);
     const platformCollusions = collusionField.generateCollusionBlocks(platformCollisions);
+    const background = new Sprite(context, { x: 0, y: 0 }, '../assets/background.png');
+    const player = new Player(context, this.scale, { x: 10, y: 10 }, { width: this.canvas.width, height: this.canvas.height }, floorCollusions);
+    const player2 = new Player(context, this.scale, { x: 50, y: 50 }, { width: this.canvas.width, height: this.canvas.height }, floorCollusions);
     function animationLoop () {
       window.requestAnimationFrame(animationLoop);
       context.fillStyle = 'grey';
@@ -49,7 +51,6 @@ class GameCanvas {
       platformCollusions.forEach((block) => {
         block.update();
       });
-      context.restore();
       player.update();
       player.velocity.x = 0;
       if (keys.left) {
@@ -58,6 +59,7 @@ class GameCanvas {
         player.velocity.x = 3;
       }
       player2.update();
+      context.restore();
     };
     animationLoop();
     window.addEventListener('keydown', (event: KeyboardEvent) => {
@@ -68,7 +70,7 @@ class GameCanvas {
           break;
         case 38:
           console.log('up');
-          player.velocity.y = -15;
+          player.velocity.y = -10;
           break;
         case 39:
           console.log('right');
@@ -90,18 +92,14 @@ class GameCanvas {
     window.addEventListener('keyup', (event: KeyboardEvent) => {
       switch (event.keyCode) {
         case 37:
-          console.log('left');
           keys.left = false;
           break;
         case 39:
-          console.log('right');
           keys.right = false;
           break;
         case 40:
-          console.log('down');
           break;
         case 32:
-          console.log('space');
           event.preventDefault();
           break;
 
--- client/src/game/mobs/Player.ts
+++ client/src/game/mobs/Player.ts
@@ -1,3 +1,5 @@
+import type CollusionBlock from '../levels/maps/CollusionBlock';
+
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 class Player {
   context: CanvasRenderingContext2D;
@@ -6,19 +8,23 @@ class Player {
   height: number;
   velocity: { x: number, y: number };
   gravity: number;
-  field: { width: number; height: number; };
+  field: { width: number, height: number };
+  collusions: CollusionBlock[];
+  scale: number;
 
-  constructor (cont: CanvasRenderingContext2D, position: { x: number, y: number }, field: { width: number, height: number }) {
+  constructor (cont: CanvasRenderingContext2D, scale: number, position: { x: number, y: number }, field: { width: number, height: number }, collusions: CollusionBlock[]) {
     this.context = cont;
     this.position = position;
     this.field = field;
-    this.width = 100;
-    this.height = 100;
+    this.scale = scale;
+    this.width = 100 / this.scale;
+    this.height = 100 / this.scale;
     this.velocity = {
       x: 0,
       y: 1
     };
     this.gravity = 0.5;
+    this.collusions = collusions;
   }
 
   draw () {
@@ -29,12 +35,62 @@ class Player {
   update () {
     this.draw();
     this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-    if (this.position.y + this.height + this.velocity.y < this.field.height) {
-      this.velocity.y += this.gravity;
-    } else {
-      this.velocity.y = 0;
+    this.checkForHorizontalCollusions();
+    this.applyGravity();
+    this.checkForVerticalCollusions();
+    // this.position.y += this.velocity.y;
+    // if (this.position.y + this.height + this.velocity.y < this.field.height) {
+    //   this.velocity.y += this.gravity;
+    // } else {
+    //   this.velocity.y = 0;
+    // }
+  }
+
+  checkForHorizontalCollusions () {
+    for (let i = 0; i < this.collusions.length; i++) {
+      const collusionBlock = this.collusions[i];
+      if (this.position.y + this.height >= collusionBlock.position.y &&
+        this.position.y <= collusionBlock.position.y + collusionBlock.height &&
+        this.position.x <= collusionBlock.position.x + collusionBlock.width &&
+        this.position.x + this.width >= collusionBlock.position.x) {
+        if (this.velocity.x > 0) {
+          this.velocity.x = 0;
+          this.position.x = collusionBlock.position.x - this.width - 0.01;
+          break;
+        }
+        if (this.velocity.x < 0) {
+          this.velocity.x = 0;
+          this.position.x = collusionBlock.position.x + collusionBlock.width + 0.01;
+          break;
+        }
+      }
     }
+  }
+
+  checkForVerticalCollusions () {
+    for (let i = 0; i < this.collusions.length; i++) {
+      const collusionBlock = this.collusions[i];
+      if (this.position.y + this.height >= collusionBlock.position.y &&
+        this.position.y <= collusionBlock.position.y + collusionBlock.height &&
+        this.position.x <= collusionBlock.position.x + collusionBlock.width &&
+        this.position.x + this.width >= collusionBlock.position.x) {
+        if (this.velocity.y > 0) {
+          this.velocity.y = 0;
+          this.position.y = collusionBlock.position.y - this.height - 0.01;
+          break;
+        }
+        if (this.velocity.y < 0) {
+          this.velocity.y = 0;
+          this.position.y = collusionBlock.position.y + collusionBlock.height + 0.01;
+          break;
+        }
+      }
+    }
+  }
+
+  applyGravity () {
+    this.position.y += this.velocity.y;
+    this.velocity.y += this.gravity;
   }
 }
 
