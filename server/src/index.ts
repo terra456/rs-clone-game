097e57a1efa0e294ebbc887e81a65c28b74c6071
terra456 <terra.456@yandex.ru>
2023-02-17 22:53:57 +0500

    fix: add 0.0.0.0 adress

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,6 @@ import app from './app';
 dotenv.config();
 const port: number = Number(process.env.PORT) || 3000;
 
-app.listen(port,'localhost', ()=> {
+app.listen(port,'0.0.0.0', ()=> {
   console.log(`Server is started on ${port} port`);
 });
