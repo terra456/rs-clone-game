6a3c6bc7446e79bc9e0bc37df0ad4730567a62db
terra456 <terra.456@yandex.ru>
2023-02-22 23:06:52 +0500

    feat: add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
+import cors from 'cors';
 import express from 'express';
 import router from './routes/router';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+
+app.options('*', cors());
 
 export default app;
