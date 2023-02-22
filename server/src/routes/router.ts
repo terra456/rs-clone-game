17551e2345fd518742eb686dd0cf1b5a497cdd3b
terra456 <52218799+terra456@users.noreply.github.com>
2023-02-22 23:50:00 +0500

    add cors into router

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,15 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import userController from '../controller/userController';
 import gamesController from '../controller/gamesController';
 
 const router = express.Router();
 
-router.use((req, res, next) => {
+router.use(cors(), (req, res, next) => {
   console.log('Time: ', Date.now());
   next();
 });
 
-router.get('/users', userController.getAllUsers);
+router.get('/users', cors(), userController.getAllUsers);
 router.post('/users', userController.createUser);
 router.get('/users/:id', userController.getUser);
 router.put('/users/:id', userController.updateUser);
