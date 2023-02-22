50813f67465f29af3fb5111aaf9df5d0b9832599
terra456 <52218799+terra456@users.noreply.github.com>
2023-02-23 00:00:35 +0500

    "fix: del cors from app.ts"

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,13 @@
-import cors from 'cors';
+// import cors from 'cors';
 import express from 'express';
 import router from './routes/router';
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
-app.options('*', cors());
+// app.options('*', cors());
 
 export default app;
