13ce1cffd1a80128d2f714b6c2a97c64b02e6475
terra456 <terra.456@yandex.ru>
2023-02-23 21:27:03 +0500

    fix: delite localhost

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,6 @@ dotenv.config();
 const port: number = Number(process.env.PORT) || 3001;
 const host: string = process.env.HOST || 'localhost';
 
-app.listen(port, host, ()=> {
-  console.log(`Server ${host} is started on ${port} port`);
+app.listen(port, ()=> {
+  console.log(`Server is started on ${port} port`);
 });
