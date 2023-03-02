15e873193ab00c75bcf626ab94bff64ce1e0fc0a
terra456 <terra.456@yandex.ru>
2023-03-03 00:21:58 +0500

    feat: fullscreen of the game

--- client/src/game/GameCanvas.ts
+++ client/src/game/GameCanvas.ts
@@ -16,7 +16,7 @@ class GameCanvas {
   scale: number;
   gameField: { width: number, height: number };
 
-  constructor (parentNode: HTMLElement = document.body, width = 600, height = 800) {
+  constructor (parentNode: HTMLElement = document.body, width = 1024, height = 600) {
     this.canvas = document.createElement('canvas');
     this.canvas.width = width;
     this.canvas.height = height;
@@ -27,8 +27,8 @@ class GameCanvas {
     //делим на высоту фона, т.е игрового поля.
     this.scale = this.canvas.height / this.gameField.height;
     this.scaledCanvas = {
-      width: width / this.scale,
-      height: height / this.scale
+      width: this.canvas.width / this.scale,
+      height: this.canvas.height / this.scale
     };
     parentNode.appendChild(this.canvas);
     this.context = this.canvas.getContext('2d');
@@ -36,17 +36,22 @@ class GameCanvas {
 
   startGame () {
     if (this.context != null) {
-      this.animate(this.context, this.canvas.width, this.canvas.height, this.scaledCanvas);
+      this.animate(this.context, { width: this.canvas.width, height: this.canvas.height }, this.scaledCanvas, this.gameField);
     }
   }
 
-  animate (context: CanvasRenderingContext2D, w: number, h: number, scaledCanvas: { width: number, height: number }) {
+  animate (
+    context: CanvasRenderingContext2D,
+    canvas: { width: number, height: number },
+    scaledCanvas: { width: number, height: number },
+    gameField: { width: number, height: number }
+  ) {
+    const scale: number = this.scale;
     const keys = {
       left: false,
       right: false,
       atack: false
     };
-    const scale: number = this.scale;
     const camera = {
       position: {
         x: 0,
@@ -56,7 +61,6 @@ class GameCanvas {
 
     let myReq: any;
     let isPaused: boolean = false;
-    let isFull: boolean = false;
 
     const gameOver = (): void => {
       cancelAnimationFrame(myReq);
@@ -78,8 +82,21 @@ class GameCanvas {
     };
 
     const changeScaling = (): void => {
-      console.log(screen);
-    }
+      if (!document.fullscreenElement) {
+        this.canvas.requestFullscreen()
+          .then(() => {
+            console.log(canvas.width, canvas.height);
+          })
+          .catch((err: Error) => {
+            console.error(`Error attempting to enable fullscreen mode: ${err.message} (${err.name})`);
+          });
+      } else {
+        document.exitFullscreen()
+          .catch((err: Error) => {
+            console.error(`Error attempting to enable fullscreen mode: ${err.message} (${err.name})`);
+          });
+      }
+    };
 
     const tilesField = new TilesField({
       context,
@@ -91,12 +108,12 @@ class GameCanvas {
     const tiles1 = tilesField.generateCollusionBlocks(layers[1].data);
     const collisionField = new CollusionField(context, 16, layers[0].width);
     const coins = collisionField.generateCollusionBlocks(layers[2].data, './assets/icons/coin.png');
-    const enemies = collisionField.generateEnemies(layers[3].data, this.gameField, tiles, tiles1);
+    const enemies = collisionField.generateEnemies(layers[3].data, gameField, tiles, tiles1);
     const background1 = new SpriteBase(context, { x: 0, y: 0 }, './assets/background/1_level/bg_1.png', 1);
-    const bgLoop = new Background(context, this.gameField);
+    const bgLoop = new Background(context, gameField);
     const bgImages = bgLoop.generate('./assets/background/1_level/mtn.png', { width: 2618, height: 571 });
-    const coinImg = new SpriteBase(context, { x: w - 120, y: 15 }, './assets/icons/coin.png');
-    const gem = new SpriteBase(context, { x: this.gameField.width - 200, y: this.gameField.height * 0.7 }, './assets/icons/gem.png');
+    const coinImg = new SpriteBase(context, { x: canvas.width - 120, y: 15 }, './assets/icons/coin.png');
+    const gem = new SpriteBase(context, { x: gameField.width - 200, y: gameField.height * 0.7 }, './assets/icons/gem.png');
     const lifeHearts: SpriteBase[] = [];
     for (let i = 0; i < 3; i++) {
       lifeHearts.push(new SpriteBase(context, { x: 30 + i * 30, y: 15 }, './assets/icons/heart.png', 0.5));
@@ -108,11 +125,11 @@ class GameCanvas {
       jump: './assets/audio/sounds/12_Player_Movement_SFX/30_Jump_03.wav',
       landing: './assets/audio/sounds/12_Player_Movement_SFX/45_Landing_01.wav',
       coin: './assets/audio/sounds/10_UI_Menu_SFX/079_Buy_sell_01.wav'
-    }
+    };
     const player = new Warior(
       context,
       { x: 10, y: 300 },
-      this.gameField,
+      gameField,
       tiles,
       tiles1,
       coins,
@@ -130,10 +147,9 @@ class GameCanvas {
     function animationLoop () {
       myReq = window.requestAnimationFrame(animationLoop);
       context.fillStyle = 'grey';
-      context.fillRect(0, 0, w, h);
+      context.fillRect(0, 0, canvas.width, canvas.height);
       context.save();
       context.scale(scale, scale);
-      // ecли scale 1, то scaledCanvas.height = this.canvas.height
       context.translate(camera.position.x, 0);
       bgImages.forEach((block) => {
         block.update();
@@ -205,8 +221,8 @@ class GameCanvas {
         }
         el.update();
       });
-
       context.restore();
+
       if (lifeHearts.length > player.lifes) {
         lifeHearts.pop();
       }
@@ -214,7 +230,7 @@ class GameCanvas {
         el.update();
       });
       coinImg.update();
-      context.strokeText(player.score.toString(), w - 50, 50);
+      context.strokeText(player.score.toString(), canvas.width - 50, 50);
     };
     animationLoop();
     window.addEventListener('keydown', (event: KeyboardEvent) => {
@@ -253,7 +269,7 @@ class GameCanvas {
         case 122:
           event.preventDefault();
           changeScaling();
-
+          break;
         default:
           console.log(event.keyCode);
           break;
