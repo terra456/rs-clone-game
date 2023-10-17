d7c5b28df3b5248c30bcf7b87f5b4f46d74bcf80
terra456 <terra.456@yandex.ru>
2023-10-17 14:09:58 +0500

    delete server

--- server/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import router from './routes/router';
-
-const app = express();
-
-app.use(express.json());
-app.use('/api', router);
-
-export default app;
--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-import app from './app';
-
-dotenv.config();
-const port: number = Number(process.env.PORT) || 3000;
-
-app.listen(port,'localhost', ()=> {
-  console.log(`Server is started on ${port} port`);
-});
